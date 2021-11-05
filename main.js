ed15978679ae0e640d40b867a4b7b6eaacf368c8
Luke Cordova <lukethegreat000@gmail.com>
2021-11-05 15:15:56 -0700

    sizing the game board

--- main.js
+++ main.js
@@ -1,23 +1,23 @@
 const canvas = document.getElementById("tetrisCanvas");
 const ctx = canvas.getContext('2d');
-
-ctx.fillStyle = 'blue';
-ctx.fillRect(10,10,10,10);
+canvas.width = 400;
+canvas.height = 700;
 
 let x = 10;
 let y = 10;
 
 var looper = setInterval(mainLoop,500);
+
 function mainLoop(){
     clearBoard();
 
     ctx.fillStyle = 'blue';
-    ctx.fillRect(x,10,10,10);
+    ctx.fillRect(x,y,10,10);
     y+=10;
 }
 
 function clearBoard(){
     ctx.fillStyle = 'white';
-    ctx.fillRect(0,0,500,500);
+    ctx.fillRect(0,0,canvas.width,canvas.height);
 
 }
